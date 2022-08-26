42092ef9110bf3bc017878b1ec344ad639eb50e2
Carlos-Teixeira-Jr <teixeirahist1988@gmail.com>
2022-08-26 11:07:28 -0300

    commit do blog

---
+++ articles/Article.js
@@ -0,0 +1,25 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/database");
+const Category = require("../categories/Category");
+
+const Article = connection.define('articles',{
+  title:{
+    type: Sequelize.STRING,
+    allowNull: false
+  }, slug: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  body: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
+})
+
+//Uma categoria tem muitos artigos
+Category.hasMany(Article);
+
+//Um artigo pertence a uma categoria
+Article.belongsTo(Category);
+
+module.exports = Article;
